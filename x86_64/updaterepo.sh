19197ef279cb75d37fba3d0e98f72dac161ae918
Erik Dubois <erik.dubois@gmail.com>
2024-03-04 16:42:31 +0100

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -7,18 +7,6 @@ repo-add -s -n -R arcolinux_repo_3party.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm arcolinux_repo_3party.db
-rm arcolinux_repo_3party.db.sig
-
-rm arcolinux_repo_3party.files
-rm arcolinux_repo_3party.files.sig
-
-mv arcolinux_repo_3party.db.tar.gz arcolinux_repo_3party.db
-mv arcolinux_repo_3party.db.tar.gz.sig arcolinux_repo_3party.db.sig
-
-mv arcolinux_repo_3party.files.tar.gz arcolinux_repo_3party.files
-mv arcolinux_repo_3party.files.tar.gz.sig arcolinux_repo_3party.files.sig
-
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
